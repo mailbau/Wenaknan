616813d8d53d8552fb7c026bc69227cdaabeb248
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-03-07 11:37:08 +0700

    feat: database connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const dotenv = require('dotenv');
-
-// const connectDB = require('./config/db');
-
+require('dotenv').config();
+const { pool } = require('./config/db');
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 8080;
 
-// connectDB();
-
-app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
