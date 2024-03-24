f9a1ec5217e28eb3fc2b7c3f6087a020bd721d13
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-03-24 21:37:27 +0700

    update endpoint

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use('/restaurants', restaurantRouter);
+app.use('/restaurant', restaurantRouter);
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
