1965ddd392eb0af51c72940b7b0a5f535e26f97d
AntoDaiva <antoniusdaiva@gmail.com>
2024-03-31 23:04:17 +0700

    feat: primary buttons and filter popup

--- frontend/pages/test/index.js
+++ frontend/pages/test/index.js
@@ -1,15 +1,16 @@
 // pages/Page.jsx
 import React from 'react';
 import Navbar from '../../components/navbar'; // Adjusted import path
+import FilterPopup from '../../components/filter';
 
 const Page = () => {
   return (
     <div>
-      <Navbar /> {/* Using Navbar component */}
-      <div className="content">
-        <h1>Welcome to My Page</h1>
-        <p>This is some content for the page.</p>
-      </div>
+        <Navbar />
+        <div className="flex flex-col justify-center w-full bg-rb-white max-md:max-w-full">
+        <FilterPopup />
+        </div>
+      
     </div>
   );
 }
