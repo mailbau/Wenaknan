6b9be53f66aaa01b0a61bb68bcee8ce86ac18f56
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-05-26 18:18:36 +0700

    fix: default page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,12 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function () {
-    return (
-        <>
-            <h1 className="text-blue-300 text-2xl font-mplus font-bold">
-                Hello World
-            </h1>
-        </>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/login');
+    }, []);
+
+    return null;
 }
